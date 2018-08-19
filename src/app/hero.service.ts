876ceeced962d2f9df12642f7153aa32f3a59510
Yibin Jiang <yibin.jiang@fit.fraunhofer.de>
2018-08-19 17:52:30 +0200

    Modify message component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,4 +15,9 @@ export class HeroService {
     return Observable.of(HEROES);
   }
 
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return Observable.of(HEROES.find(hero => hero.id === id));
+  }
+
 }
